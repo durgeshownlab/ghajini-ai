5b87b39d46629d5fb6a2660168b02cb97a7424e2
Durgesh Singh <durgeshownlab@gmail.com>
2024-10-12 01:37:26 +0530

    added meta tag

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
   const [promptText, setPromptText] = useState<string>("");
   const [isInputDisabled, setIsInputDisabled] = useState<boolean>(false);
   const [isSubmitBtnDisabled, setIsSubmitBtnDisabled] = useState<boolean>(true);
-  const [messages, setMessages] = useState<Message[]>([{text: "Hello folks, I am Ghajini Ai, I always forgot the thing, How can i help you!", isUser: false}]); // Store messages
+  const [messages, setMessages] = useState<Message[]>([{text: "Hello! I’m Ghajini AI, your AI assistant with a short-term memory loss for your privacy. How can i help you today?", isUser: false}]); // Store messages
   const chatContainer = useRef<HTMLDivElement>(null);
   const chatInput = useRef<HTMLInputElement>(null);
 
